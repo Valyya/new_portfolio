5f09d341a0eda1df932047fc6a71a2513334dd34
Valyya <Valyya@ukr.net>
2023-06-04 18:02:00 +0300

    002

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+// import * as flsFunctions from "./modules/functions.js";
+
+// flsFunctions.isWebp();
+
+/*
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
+*/
+
+// console.log("2222222222");
+
+// function toggleSideMenu() {
+//   document.getElementById("side-menu").classList.toggle("active");
+// }
